6ee6dbc6e2a0a02d381678d28f3302515bc4af80
Shreya Patel <patelshreya.v.1806@gmail.com>
2026-09-14 15:34:30 +0530

    ads

--- python/p3.py
+++ python/p3.py
@@ -1,5 +1,50 @@
-try:
-    age = int(input("Enter age: "))
-    print(age)
-except ValueError:
-    print("Please enter a valid number.")
+# class BankAccount:
+#     def __init__(self, balance):
+#         self._balance = balance
+
+#     def deposit(self, amount):
+#         if amount < 0:
+#             raise ValueError("Amount must be positive")
+#         elif amount == 0:
+#             raise ValueError("Amount must be greater than zero")
+#         elif amount >= 10000:
+#             raise ValueError("Amount must not exceed 10,000")
+        
+#         self._balance += amount
+
+#     def get_balance(self):
+#         return self._balance
+
+# account = BankAccount(1000)
+
+# account.deposit(-1000)
+
+# print(account.get_balance())
+
+
+# import asyncio
+# async def get_customer():
+#     await asyncio.sleep(2)
+#     return "Amit"
+
+# async def main():
+#     customer = await get_customer()
+#     print(customer)
+
+# asyncio.run(main())
+
+
+
+from pydantic import BaseModel
+class Customer(BaseModel):
+    name: str
+    age: int
+    email: str
+
+customer = Customer(
+    name="Amit",
+    age= 25, 
+    email="shreya@gmail.com"
+)
+
+print(customer)
