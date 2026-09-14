0973326b71f34a12def1ec7dfba12c70e862f249
Shreya Patel <patelshreya.v.1806@gmail.com>
2026-09-14 15:41:51 +0530

    commit

--- python/p3.py
+++ python/p3.py
@@ -1,40 +1,44 @@
-# class BankAccount:
-#     def __init__(self, balance):
-#         self._balance = balance
-
-#     def deposit(self, amount):
-#         if amount < 0:
-#             raise ValueError("Amount must be positive")
-#         elif amount == 0:
-#             raise ValueError("Amount must be greater than zero")
-#         elif amount >= 10000:
-#             raise ValueError("Amount must not exceed 10,000")
-        
-#         self._balance += amount
-
-#     def get_balance(self):
-#         return self._balance
-
-# account = BankAccount(1000)
-
-# account.deposit(-1000)
-
-# print(account.get_balance())
-
-
-# import asyncio
-# async def get_customer():
-#     await asyncio.sleep(2)
-#     return "Amit"
-
-# async def main():
-#     customer = await get_customer()
-#     print(customer)
-
-# asyncio.run(main())
+def devider(name , chapter):
+    length = f"{name} is reading program {chapter}"
+    print("\n")
+    print(length)
+    print("=" * len(length))
 
 
 
+devider("Python", 1)
+class BankAccount:
+    def __init__(self, balance):
+        self._balance = balance
+    def deposit(self, amount):
+        if amount < 0:
+            raise ValueError("Amount must be positive")
+        elif amount == 0:
+            raise ValueError("Amount must be greater than zero")
+        elif amount >= 10000:
+            raise ValueError("Amount must not exceed 10,000")
+        self._balance += amount
+    def get_balance(self):
+        return self._balance
+account = BankAccount(1000)
+account.deposit(1000)
+print(account.get_balance())
+
+
+
+devider("Python", 2)
+import asyncio
+async def get_customer():
+    await asyncio.sleep(2)
+    return "Amit"
+async def main():
+    customer = await get_customer()
+    print(customer)
+asyncio.run(main())
+
+
+
+devider("Python", 3)
 from pydantic import BaseModel
 class Customer(BaseModel):
     name: str
@@ -48,3 +52,4 @@ customer = Customer(
 )
 
 print(customer)
+print("\n")
