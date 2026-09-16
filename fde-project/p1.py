e9d1f3cebc9deb930c6f3f63105fb4ad3d324d5e
Shreya Patel <patelshreya.v.1806@gmail.com>
2026-09-16 12:25:51 +0530

    wawdd

--- fde-project/p1.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open(r"d:\my_projects\fde-project\customers.csv", "r", newline="", encoding="utf-8") as file:
-    reader = csv.DictReader(file)
-
-    for customer in reader:
-        print(customer["id"],"\t",customer["name"],"\t",customer["city"])
-        # print(customer["name"],"\t",customer["city"])
